a2e3659bbdda75d6b66dfc5817376859052228d1
Yaryna93 <122755101+Yaryna93@users.noreply.github.com>
2023-07-23 09:48:34 +0000

    Deploying to gh-pages from @ Yaryna93/goit-js-hw-09@74d67b24e18a2c77963fde8fb88c3436743e065b 🚀

--- 01-color-switcher.154f259b.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,t.disabled=!0,clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.154f259b.js.map
